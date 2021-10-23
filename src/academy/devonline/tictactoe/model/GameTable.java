d15c388065985caa636177f25f426753eaa6b748
horodniuk <maxim.gorodnyk55@gmail.com>
2021-10-23 23:55:25 +0300

    Override toString methods for model classes

--- src/academy/devonline/tictactoe/model/GameTable.java
+++ src/academy/devonline/tictactoe/model/GameTable.java
@@ -42,5 +42,17 @@ public class GameTable {
         table[cell.getRow()][cell.getCol()] = sign;
     }
 
-
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("GameTable{");
+        sb.append("table=");
+        for (int i = 0; i < table.length; i++) {
+            sb.append(Arrays.toString(table[i]));
+            if (i < table.length - 1) {
+                sb.append(';');
+            }
+        }
+        sb.append('}');
+        return sb.toString();
+    }
 }
