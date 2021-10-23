2f489de9a19545554c8ba8ffc3cd40655150894a
horodniuk <maxim.gorodnyk55@gmail.com>
2021-10-24 02:15:21 +0300

    Implement DataPrinter component

--- src/academy/devonline/tictactoe/component/DataPrinter.java
+++ src/academy/devonline/tictactoe/component/DataPrinter.java
@@ -16,6 +16,7 @@
 
 package academy.devonline.tictactoe.component;
 
+import academy.devonline.tictactoe.model.Cell;
 import academy.devonline.tictactoe.model.GameTable;
 
 /**
@@ -25,9 +26,34 @@ import academy.devonline.tictactoe.model.GameTable;
  */
 public class DataPrinter {
     public void printMappingTable() {
-
+        char[][] mappingTable = {
+                {'7', '8', '9'},
+                {'4', '5', '6'},
+                {'1', '2', '3'}
+        };
+        printGameTable(mappingTable);
     }
 
-    public void printGameTable(final GameTable gameTable) {
+    public void printGameTable( GameTable gameTable) {
+        for (int i = 0; i < 3; i++) {
+            System.out.println("----------");
+            for (int j = 0; j < 3; j++) {
+                System.out.print("|" + gameTable.getSign(new Cell(i, j))+ " ");
+            }
+            System.out.println("|");
+        }
+        System.out.println("----------");
     }
+
+    public void printGameTable( char[][] mappingTable) {
+        for (int i = 0; i < 3; i++) {
+            System.out.println("----------");
+            for (int j = 0; j < 3; j++) {
+                System.out.print("|" + mappingTable[i][j]+ " ");
+            }
+            System.out.println("|");
+        }
+        System.out.println("----------");
+    }
+
 }
