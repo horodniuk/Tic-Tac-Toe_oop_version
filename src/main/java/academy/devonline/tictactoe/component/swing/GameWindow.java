bc25b7aa229c1f9851d79964c79a83d79aae0b6b
horodniuk <maxim.gorodnyk55@gmail.com>
2021-11-19 19:30:24 +0200

    Add GameWindow.java

---
+++ src/main/java/academy/devonline/tictactoe/component/swing/GameWindow.java
@@ -0,0 +1,165 @@
+/*
+ * Copyright (c) 2019. http://devonline.academy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package academy.devonline.tictactoe.component.swing;
+
+import academy.devonline.tictactoe.component.DataPrinter;
+import academy.devonline.tictactoe.component.UserInputReader;
+import academy.devonline.tictactoe.component.move.user.mapping.MappingTable;
+import academy.devonline.tictactoe.model.Cell;
+import academy.devonline.tictactoe.model.GameTable;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+/**
+ * @author Maksym Horodniuk
+ * @link http://devonline.academy/java
+ * tic-tac-toe
+ */
+public class GameWindow extends JFrame implements DataPrinter, UserInputReader {
+    private static final int GAME_TABLE_SIZE = 3;
+
+    private static final int FONT_SIZE = 35;
+
+    private static final int CELL_SIZE = 150;
+
+    private final JLabel[][] cells = new JLabel[GAME_TABLE_SIZE][GAME_TABLE_SIZE];
+
+    private Cell clickedCell;
+
+    public GameWindow() {
+        super("Tic-tac-toe");
+        setSystemLookAndFeel();
+        setLayout(new GridLayout(GAME_TABLE_SIZE, GAME_TABLE_SIZE));
+        createGameTable();
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
+        pack();
+        displayInTheMiddleOfTheScreen();
+    }
+
+    private void setSystemLookAndFeel() {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (final ClassNotFoundException | UnsupportedLookAndFeelException |
+                IllegalAccessException | InstantiationException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private void createGameTable() {
+        for (int i = 0; i < GAME_TABLE_SIZE; i++) {
+            for (int j = 0; j < GAME_TABLE_SIZE; j++) {
+                final JLabel label = new JLabel();
+                cells[i][j] = label;
+                label.setPreferredSize(new Dimension(CELL_SIZE, CELL_SIZE));
+                label.setHorizontalAlignment(SwingConstants.CENTER);
+                label.setVerticalAlignment(SwingConstants.CENTER);
+                label.setFont(new Font(Font.SERIF, Font.PLAIN, FONT_SIZE));
+                label.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+                add(label);
+                final Cell cell = new Cell(i, j);
+                label.addMouseListener(new MouseAdapter() {
+                    @Override
+                    public void mouseClicked(final MouseEvent unused) {
+                        synchronized (GameWindow.this) {
+                            clickedCell = cell;
+                            GameWindow.this.notifyAll();
+                        }
+                    }
+                });
+            }
+        }
+    }
+
+    private void displayInTheMiddleOfTheScreen() {
+        final Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        setLocation(dim.width / 2 - getSize().width / 2, dim.height / 2 - getSize().height / 2);
+        setVisible(true);
+    }
+
+
+    @Override
+    public void printInstructionMessage() {
+        final String message = "Use the following mapping table to specify a cell using numbers from 1 to 9:";
+        JOptionPane.showMessageDialog(this, message, "Info", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    @Override
+    public void printInputInstruction() {
+        final String message = "Please type number between 1 and 9:";
+        JOptionPane.showMessageDialog(this, message, "Info", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    @Override
+    public void printNotEmptyCell() {
+        final String message = "Can't make a move, because the cell is not free! Try again!";
+        JOptionPane.showMessageDialog(this, message, "Info", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    @Override
+    public void printWinner(final String player) {
+        final String message = player + " WIN!";
+        JOptionPane.showMessageDialog(this, message, "Info", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    @Override
+    public void printGameOver() {
+        final String message = "GAME OVER!";
+        JOptionPane.showMessageDialog(this, message, "Info", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    @Override
+    public void printDraw() {
+        final String message = "Sorry, DRAW!";
+        JOptionPane.showMessageDialog(this, message, "Info", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    @Override
+    public void printGameTable(final GameTable gameTable) {
+        for (int i = 0; i < GAME_TABLE_SIZE; i++) {
+            for (int j = 0; j < GAME_TABLE_SIZE; j++) {
+                cells[i][j].setText(String.valueOf(gameTable.getSign(new Cell(i, j))));
+            }
+        }
+    }
+
+    @Override
+    public void printGameTable(final MappingTable mappingTable) {
+
+    }
+
+    @Override
+    public Cell tryGetUserCell() {
+        synchronized (this) {
+            try {
+                wait();
+            } catch (final InterruptedException exception) {
+                exception.printStackTrace();
+                System.exit(2);
+            }
+        }
+        return clickedCell;
+    }
+
+
+   /* public static void main(String[] args) {
+        new GameWindow();
+    }*/
+}
